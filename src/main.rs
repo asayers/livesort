0cc6ee43a6d7377b8b6541a20c546f70b5d06a05
Alex Sayers <alex@asayers.com>
2020-05-29 14:18:18 +0900

    Don't print more lines than the terminal has

--- src/main.rs
+++ src/main.rs
@@ -18,20 +18,32 @@ fn main_2() -> Result<()> {
     let stdin = BufReader::new(stdin.lock());
     let mut stdout = BufWriter::new(stdout.lock());
     let mut buf = vec![];
+    let mut printed = 0;
+    macro_rules! clear {
+        () => {
+            // We don't want to clear any line if we haven't printed yet
+            if printed > 0 {
+                stdout
+                    .queue(MoveToPreviousLine(printed as u16))?
+                    .queue(Clear(ClearType::FromCursorDown))?;
+            }
+        };
+    }
     for line in stdin.lines() {
         let line = line?;
-        if !buf.is_empty() {
-            // We don't want to clear any line if we haven't printed yet
-            stdout
-                .queue(MoveToPreviousLine(buf.len() as u16))?
-                .queue(Clear(ClearType::FromCursorDown))?;
-        }
         buf.push(line);
         buf.sort();
-        for l in &buf {
+        clear!();
+        printed = buf.len().min(usize::from(size()?.1 - 1));
+        for l in buf.iter().skip(buf.len() - printed) {
             stdout.queue(Print(l))?.queue(Print("\n"))?;
         }
         stdout.flush()?;
     }
+    clear!();
+    for l in &buf {
+        stdout.queue(Print(l))?.queue(Print("\n"))?;
+    }
+    stdout.flush()?;
     Ok(())
 }
