77094a0ef95e8ec77e06a753d44c8c17154e1f35
Alex Sayers <alex@asayers.com>
2021-01-29 15:42:56 +0900

    Make the set into a multiset

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crossterm::{cursor, terminal, terminal::ClearType, QueueableCommand};
-use std::collections::BTreeSet;
+use std::collections::BTreeMap;
 use std::error::Error;
 use std::io::{stdin, stdout, BufRead, Write};
 use std::time::{Duration, Instant};
@@ -16,20 +16,20 @@ struct Opts {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opts = Opts::from_args();
-    let mut vals = BTreeSet::new();
+    let mut vals = BTreeMap::<String, u16>::new();
     let mut last_print_rows = 0;
     let mut last_print_time = Instant::now();
     let out = stdout();
     let mut out = out.lock();
     out.write_all(b"\n")?; // Why? I don't know...
     for line in stdin().lock().lines() {
-        vals.insert(line.unwrap());
+        *vals.entry(line.unwrap()).or_default() += 1;
         if last_print_time.elapsed() > Duration::from_millis(1000 / FPS) {
             out.queue(cursor::MoveToPreviousLine(last_print_rows))?
                 .queue(terminal::Clear(ClearType::FromCursorDown))?;
             let (_, height) = terminal::size()?;
             last_print_rows = (height - 1).min(vals.len() as u16);
-            for val in vals.iter().take(last_print_rows as usize) {
+            for val in vals.keys().take(last_print_rows as usize) {
                 out.write_all(val.as_bytes())?;
                 out.write_all(b"\n")?;
             }
@@ -39,7 +39,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     out.queue(cursor::MoveToPreviousLine(last_print_rows))?
         .queue(terminal::Clear(ClearType::FromCursorDown))?;
-    for val in &vals {
+    for val in vals.keys() {
         out.write_all(val.as_bytes())?;
         out.write_all(b"\n")?;
     }
