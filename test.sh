92c96bef92e9b16216e878b2cfc23b07528d11b3
Alex Sayers <alex@asayers.com>
2021-01-31 16:44:01 +0900

    Compare output to sort/uniq in test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -eu
 cargo b
-echo STARTING
-(sleep 1; seq 2 5; seq 1 6) | while read line; do echo $line; sleep 1; done | ./target/debug/livesort
+
+input () {
+    (seq 2 5; seq 1 6) | while read line; do echo "The next number, if you'd believe it, really is $line"; done
+}
+slow() { while read line; do echo "$line"; sleep 1; done; }
+
+echo "DIFF livesort vs sort"
+diff <(input | ./target/debug/livesort) <(input | sort) && echo "OK"
+echo "DIFF livesort -u vs sort | uniq"
+diff <(input | ./target/debug/livesort -u) <(input | sort | uniq) && echo "OK"
+echo "DIFF livesort -c vs sort | uniq -c"
+diff <(input | ./target/debug/livesort -c) <(input | sort | uniq -c) && echo "OK"
+
+echo
+echo RUNNING livesort
+(sleep 1; input) | slow | ./target/debug/livesort -c
