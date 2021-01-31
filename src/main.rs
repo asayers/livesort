asayers/livesort

commit 336e92cf77184af360d51237fcee17f3f5ade6e9

Remove the strange "initial newline" behaviour

src/main.rs
@@ -37,14 +37,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut last_print_time = Instant::now();
     let out = stdout();
     let mut out = out.lock();
-    if out.is_tty() {
-        out.write_all(b"\n")?; // Why? I don't know...
-    }
     for line in stdin().lock().lines() {
         *vals.entry(line.unwrap()).or_default() += 1;
         if last_print_time.elapsed() > Duration::from_millis(1000 / FPS) && out.is_tty() {
-            out.queue(cursor::MoveToPreviousLine(last_print_rows))?
-                .queue(terminal::Clear(ClearType::FromCursorDown))?;
+            if last_print_rows != 0 {
+                // Looks like MoveToPreviousLine(0) still moves up one line,
+                // so we need to guard the 0 case
+                out.queue(cursor::MoveToPreviousLine(last_print_rows))?
+                    .queue(terminal::Clear(ClearType::FromCursorDown))?;
+            }
             let (_, height) = terminal::size()?;
             let len = if opts.uniq {
                 vals.len()

commit 45a889ae923bdfc03c65531436e00f2ff35d8425

Don't clobber a line when there's no live preview
This bug was introduced by 336e92c.

src/main.rs
@@ -49,8 +49,10 @@ fn main() -> Result<(), Box<dyn Error>> {
             last_print_time = Instant::now();
         }
     }
-    out.queue(cursor::MoveToPreviousLine(last_print_rows))?
-        .queue(terminal::Clear(ClearType::FromCursorDown))?;
+    if last_print_rows != 0 {
+        out.queue(cursor::MoveToPreviousLine(last_print_rows))?
+            .queue(terminal::Clear(ClearType::FromCursorDown))?;
+    }
     print_vals(opts, &vals, 0, &mut out)?;
     out.flush()?;
     Ok(())