9333598368b0af1596be7fc0395902cd5ee16121
Alex Sayers <alex@asayers.com>
2021-01-31 13:01:43 +0900

    Make the output of test.sh more predictable

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -eu
-cat Cargo.lock | while read line; do echo $line; sleep 0.2; done | cargo run
+cargo b
+echo STARTING
+(sleep 1; seq 2 5; seq 1 6) | while read line; do echo $line; sleep 1; done | ./target/debug/livesort
