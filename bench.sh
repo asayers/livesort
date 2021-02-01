5a56699c1c09d4b43aef428518c4086b06cee391
Alex Sayers <alex@asayers.com>
2021-02-01 16:56:44 +0900

    Include the installed version in the benchmark script

--- bench.sh
+++ bench.sh
@@ -2,7 +2,9 @@
 cargo b --release
 export LC_ALL=C
 FILE=Cargo.lock
-livesort="./target/release/livesort -c <$FILE"
+livesort_installed="livesort -c <$FILE"
+livesort_head="./target/release/livesort -c <$FILE"
 sort="sort <$FILE | uniq -c"
-cmp <(eval $livesort) <(eval $sort) && echo "sanity check ok" >&2
-cbdr sample "livesort:$livesort" "sort:$sort"
+cmp <(eval $livesort_head) <(eval $sort) && echo "sanity check (HEAD): ok" >&2
+cmp <(eval $livesort_installed) <(eval $sort) && echo "sanity check (installed): ok" >&2
+cbdr sample "sort:$sort" "livesort_installed:$livesort_installed" "livesort_HEAD:$livesort_head"
